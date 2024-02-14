0860ab22302c812732fe301bbc3921de7915c72c
Jorge Orozco-Otero <jorgeorozcootero@gmail.com>
2024-02-14 13:52:49 -0800

    feat: error middleware created.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
+const { errorHandler } = require('./middleware/errorMiddleware')
 
 
 const app = express()
