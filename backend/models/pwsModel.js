91d61d829191ed412bb7081aef50a180268cb262
Jorge Orozco-Otero <jorgeorozcootero@gmail.com>
2024-02-14 14:36:07 -0800

    feat: password model created.

---
+++ backend/models/pwsModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const pwSchema = mongoose.Schema({
+    company: {
+        type: String,
+        require: [true, 'Please, type the name of the company.']
+    },
+    username: {
+        type: String,
+        require: [true, 'Please, type your username.']
+    },
+    email: {
+        type: String
+    },
+    password: {
+        type: String,
+        required: [true, 'Please, type your password.']
+    }
+},{
+    timestamps: true
+})
+
+module.exports = mongoose.model('Pw', pwSchema)
